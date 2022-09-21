e9422475dd33d6d63f8d14e640562206e77852df
Jeff Moore <jeoffmore+git@gmail.com>
2022-09-21 17:51:03 -0400

    expanded times to JSON string (RFC 3339 date) or int64

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ var requiredEnvs = [...]string{
 func HandleLambdaEvent(ctx context.Context, event json.RawMessage) error {
 
 	// Generate current time for later use
-	t := time.Now().UnixMilli()
+	t := time.Now()
 
 	// Validate json
 	if !json.Valid(event) {
@@ -48,19 +48,19 @@ func HandleLambdaEvent(ctx context.Context, event json.RawMessage) error {
 		return fmt.Errorf("Required environment variables not set. Please ensure all of the following are set: %s\n", requiredEnvs)
 	}
 
-	// Attempt Unmarshal into PostAnnotationReq
 	var par PostAnnotationReq
+
 	/* TODO - leverage https://pkg.go.dev/encoding/json#Decoder.DisallowUnknownFields to throw errors when
-	invalid fields are passed to the PostAnnotationReq. By default, these fields are ignored
+	invalid fields are passed to the PostAnnotationReq. By default, these fields are discarded
 	*/
 	err := json.Unmarshal(event, &par)
 	if err != nil {
 		return err
 	}
 
-	// Assign time if it doesn't exist
-	if par.TimeStart == 0 {
-		par.TimeStart = t
+	err = par.setTimes(t)
+	if err != nil {
+		return err
 	}
 
 	// Validate GRAFANA_URL
@@ -70,8 +70,8 @@ func HandleLambdaEvent(ctx context.Context, event json.RawMessage) error {
 	// TODO - Validate GRAFANA_TOKEN
 
 	// Validate PostAnnotationReq
-	if !par.IsValid() {
-		return fmt.Errorf("Invalid request. Please ensure all fields in '%s' are set.\n", postAnnotationsDocURL)
+	if !par.isValid() {
+		return fmt.Errorf("Invalid request. Please ensure all fields required by '%s' are set. Input: '%+v'\n", postAnnotationsDocURL, par)
 	}
 
 	// Handle Grafana logic
@@ -136,6 +136,7 @@ func postAnnotationHandler(ctx context.Context, grafana GrafanaAuth, par PostAnn
 		return fmt.Errorf(textResponse)
 
 	} else {
+		// TODO - there is no stdout, one needs to pass a response object to the lambda
 		fmt.Println(textResponse)
 	}
 
@@ -157,28 +158,99 @@ func postAnnotationHandler(ctx context.Context, grafana GrafanaAuth, par PostAnn
 // See https://github.com/grafana/grafana/blob/fe87ffdda0d388cf65a3eeafdbf92a5b1b621079/pkg/api/dtos/annotations.go#L5
 
 type PostAnnotationReq struct {
-	DashboardId  int64  `json:"dashboardId"`
-	DashboardUID string `json:"dashboardUID,omitempty"`
-	PanelId      int64  `json:"panelId,omitempty"`
-	TimeEnd      int64  `json:"timeEnd,omitempty"`
+	DashboardId  int64       `json:"dashboardId,omitempty"`
+	DashboardUID string      `json:"dashboardUID,omitempty"`
+	PanelId      int64       `json:"panelId,omitempty"`
+	TimeEnd      interface{} `json:"timeEnd,omitempty"`
 	// required
-	Tags      []string `json:"tags"`
-	Text      string   `json:"text"`
-	TimeStart int64    `json:"time"`
+	Tags      []string    `json:"tags"`
+	Text      string      `json:"text"`
+	TimeStart interface{} `json:"time"`
 }
 
 // IsValid Validate PostAnnotationReq prior to attempting to send to Grafana
-func (PostAnnotationReq) IsValid() bool {
-	/* TODO - implement this
-	One could leverage the underlying Grafana API for validation, but this reduces Lambda time
-	and feels like a better way to interact with a well-documented API
+func (req *PostAnnotationReq) isValid() bool {
+	/*
+		One could leverage the underlying Grafana API for validation and that would reduce Lambda time,
+		but this feels like a better way to interact with a well-documented API
 	*/
+
+	// TimeStart and TimeEnd start types start as interface{} but MUST end up int64 for the API call
+	switch req.TimeStart.(type) {
+	// It is fine that req.TimeStart  and req.TimeEnd have an underlying float64 type since
+	//	the json representation of the underlying {}interface of type int64 (per setTime) will render correctly
+	case float64:
+	case int64:
+	default:
+		return false
+	}
+	switch req.TimeEnd.(type) {
+	case nil:
+	case float64:
+	case int64:
+	default:
+		return false
+	}
+
 	return true
 }
 
-// Struct holding Grafana auth stuff. Will hold different credential types in the future. See some upstream api for reference
-
+// GrafanaAuth Struct holding Grafana auth stuff. Will hold different credential types in the future. See some upstream api for reference
 type GrafanaAuth struct {
 	URL   string
 	Token string
+}
+
+// set i as time-parsed int64 in UnixMilli
+func setTime(i interface{}, s string) (t time.Time, err error) {
+	// See https://pkg.go.dev/encoding/json#Unmarshal for available types on a Unmarshalled interface{}
+	switch timeType := i.(type) {
+	case float64:
+		f, ok := i.(float64)
+		if !ok {
+			return time.UnixMilli(0), fmt.Errorf("Unable to convert '%v' into a float64 in field '%s'\n", f, s)
+		}
+		// Changing the {}interface to a different type is not necessary since the underlying type changes
+		i = int64(f)
+		t = time.UnixMilli(i.(int64))
+	case string:
+		t, err = time.Parse(time.RFC3339, i.(string))
+		if err == nil {
+			i = t.UnixMilli()
+		}
+	// err is handled in the calling function
+	default:
+		err = fmt.Errorf("input type '%s' on field '%s' not valid for parsing, please use RFC3339", timeType, s)
+	}
+
+	return t, err
+}
+
+// Handle time setting logic according to upstream Grafana docs. Handle string and int64 input in req
+func (req *PostAnnotationReq) setTimes(t time.Time) error {
+	var startErr, endErr error
+	var startTime, endTime time.Time
+	// Handle TimeStart, which needs to exist
+	if req.TimeStart == nil {
+		req.TimeStart = t.UnixMilli()
+	} else {
+		// This and endTime are set correctly because interface{} is always passed by reference
+		startTime, startErr = setTime(req.TimeStart, "time")
+	}
+	// Handle TimeEnd, which does not need to exist
+	if req.TimeEnd != nil {
+		endTime, endErr = setTime(req.TimeEnd, "timeEnd")
+	}
+
+	// Return combined errors
+	if startErr != nil || endErr != nil {
+		// Wrapping errors may be preferred in the future
+		return fmt.Errorf("%s; %s", startErr, endErr)
+	}
+	// Check for time consistency
+	if startTime.After(endTime) {
+		return fmt.Errorf("start time '%s' is after end time '%s'\n", startTime, endTime)
+	}
+
+	return nil
 }
