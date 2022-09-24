3f72d846f3bac5e1a18378ce3bb4ea878f12e437
Jeff Moore <jeoffmore+git@gmail.com>
2022-09-24 14:11:54 -0400

    Added aws-user-role-policy Terraform module

---
+++ deploy/terraform/modules/aws-user-role-policy/variables.tf
@@ -0,0 +1,16 @@
+// Name of the created IAM user
+variable "iam_user" {}
+
+// Tags set on applicable resources
+variable "tags" {
+  default = {}
+}
+
+// IAM policy document
+variable "iam_policy" {}
+
+// Determines whether or not an IAM Access Key is provisioned for the IAM user. Access Key will exist in the statefile
+variable "iam_access_key" {
+  type = bool
+  default = false
+}
